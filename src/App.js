8c54779161a5b4a015167343f2e91d8f58eaf6ce
Hadiya-bilal <hadiya@tlu.ee>
2025-03-19 02:36:09 +0200

    Initial commit: Set up Tic-Tac-Toe project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Board from './Components/Board';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Tic-Tac-Toe</h1>
+      <Board />
+    </div>
+  );
+}
+
+export default App;
